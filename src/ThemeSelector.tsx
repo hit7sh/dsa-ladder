741f7e823e239554e5a877c27533a5f90332a7b7
hit7sh <hit7sh@gmail.com>
2024-11-09 16:24:00 +0530

    theme-selector

---
+++ src/ThemeSelector.tsx
@@ -0,0 +1,30 @@
+import { Button } from "@/components/ui/button"
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuGroup,
+    DropdownMenuItem,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import { EDITOR_THEMES } from './constants'
+
+const ThemeSelector = ({ setTheme }: { setTheme: any }) => {
+    return (
+        <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+                <Button variant="secondary">Change Theme🔽</Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className="w-56">
+                <DropdownMenuGroup>
+                    {EDITOR_THEMES.map((theme) => (
+                        <DropdownMenuItem key={theme} onClick={() => setTheme(theme)}>
+                            <span>{theme}</span>
+                        </DropdownMenuItem>
+                    ))}
+                </DropdownMenuGroup>
+            </DropdownMenuContent>
+        </DropdownMenu>
+    )
+}
+
+export default ThemeSelector
