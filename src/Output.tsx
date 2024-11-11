e687d00b69c20be0d868eaf18d3db1332be6b377
hit7sh <hit7sh@gmail.com>
2024-11-11 17:29:03 +0530

    output-color

--- src/Output.tsx
+++ src/Output.tsx
@@ -5,7 +5,7 @@ const Output = ({ output }: { output: string }) => {
     return (
         <>
             <div className="bg-blue-500 mt-1 pl-2"> OUTPUT </div>
-            <div className="bg-slate-500 pl-2" style={{ whiteSpace: "pre-line" }}>
+            <div className="bg-blue-200 pl-2" style={{ whiteSpace: "pre-line" }}>
                 {output}
             </div>
         </>
