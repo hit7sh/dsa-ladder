6d80dcc9e943a61b562ef4466a845b5348a2e060
hit7sh <hit7sh@gmail.com>
2024-11-11 13:31:53 +0530

    adds compiler errors, runtime errors

--- src/CodeErrors.tsx
+++ src/CodeErrors.tsx
@@ -1,6 +1,9 @@
-import React from 'react'
+interface CodeErrorsPropsT {
+    compile_errors: undefined | string;
+    runtime_errors: undefined | string;
+};
 
-const CodeErrors = ({ compile_errors, runtime_errors }) => {
+const CodeErrors = ({ compile_errors, runtime_errors }: CodeErrorsPropsT) => {
     return (
         <>
             <div className="bg-red-400 mt-1 pl-2"> OUTPUT </div>
