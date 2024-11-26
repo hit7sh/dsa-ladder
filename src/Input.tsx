f79009d1a06482b6ef0d5220b284917974adb358
hit7sh <hit7sh@gmail.com>
2024-11-27 02:01:09 +0530

    fix

--- src/Input.tsx
+++ src/Input.tsx
@@ -6,6 +6,7 @@ const Input = ({ inputText, setInputText }: { inputText: string, setInputText: a
             <Textarea
                 className="bg-blue-200 h-24 border-4"
                 value={inputText}
+                placeholder="Write custom input here..."
                 onChange={(e) => setInputText(e.target.value)}
             />
         </>
